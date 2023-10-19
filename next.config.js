685e4742bac986bde343ad1b18223b9636559a73
hdeg <76611318+exit1991@users.noreply.github.com>
2023-10-19 18:29:43 +0900

    設定ファイル更新 #11

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: false, // 厳格モード (2回レンダリングをやめる場合は false にする)
+  //   output: 'export', // 静的サイトビルドの場合は指定する
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
