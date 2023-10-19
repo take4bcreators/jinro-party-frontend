d5db9a18fb3133f101b6cbb5c3cc73cb495a9516
saito <143883407+MasashiSaitou@users.noreply.github.com>
2023-10-19 21:05:12 +0900

    判定処理追加 #1

--- src/app/pl/page.tsx
+++ src/app/pl/page.tsx
@@ -1,7 +1,12 @@
 'use client';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { makeAPIEndpointURL, execGETGameState } from '@/utils/api';
+import { APIRouting } from '@/config/apiRouting';
+import { useRouter } from 'next/navigation';
 
 export default function Home(): JSX.Element {
+  const router = useRouter();
+
   useEffect(() => {
     // デバイスIDの確認と生成
     const deviceId = localStorage.getItem('jrpt_general_deviceid');
@@ -11,9 +16,44 @@ export default function Home(): JSX.Element {
     }
   }, []);
 
+  const [gameState, setGameState] = useState('');
+  const [apiEndpointURL, setApiEndpointURL] = useState('');
+
+  useEffect(() => {
+    const endpointURL = makeAPIEndpointURL(APIRouting.Point.getGameState);
+    setApiEndpointURL(endpointURL);
+  }, []);
+
+  useEffect(() => {
+    execGETGameState(apiEndpointURL).then((resValue) => {
+      if (resValue == undefined) {
+        return;
+      }
+      setGameState(resValue);
+    });
+  }, [apiEndpointURL]);
+
+  if (gameState === '') {
+    return <></>;
+  }
+
+  if (gameState === 'PreGame') {
+    console.log('募集前');
+    return (
+      <>
+        <h1>人狼パーティ</h1>
+        <p>募集前です...</p>
+        <p>しばらくお待ちください</p>
+      </>
+    );
+  } else {
+    console.log('ゲーム中');
+    router.push('/pl/playing/');
+  }
+
   return (
     <>
-      <p>プレイヤーサイトです</p>
+      <p>ロード中...</p>
     </>
   );
 }
---
+++ src/app/pl/playing/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+export default function Home(): JSX.Element {
+  return (
+    <>
+      <p>ゲーム中です...</p>
+      <p>ここにゲーム画面が表示される</p>
+      <p>ゲーム画面</p>
+      <p>ゲーム画面</p>
+      <p>ゲーム画面</p>
+      <p>ゲーム画面</p>
+    </>
+  );
+}
