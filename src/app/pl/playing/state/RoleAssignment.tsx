31f1b4e0623f1fb44a6d1e33d868ca3d8e3cd9b6
hdeg <76611318+exit1991@users.noreply.github.com>
2023-10-29 13:47:58 +0900

    HTMLの誤りを修正 #20

--- src/app/pl/playing/state/RoleAssignment.tsx
+++ src/app/pl/playing/state/RoleAssignment.tsx
@@ -2,11 +2,9 @@ export default function Home(): JSX.Element {
   return (
     <>
       <h1>役職発表</h1>
-      <p>
-        <p>あなたは...</p>
-        <p>〇〇</p>
-        <p>です。</p>
-      </p>
+      <p>あなたは...</p>
+      <p>〇〇</p>
+      <p>です。</p>
     </>
   );
 }
