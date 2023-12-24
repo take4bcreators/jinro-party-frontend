cad3a4bc9e90e8b49c12a6aa3c487844965d8956
hdeg <76611318+exit1991@users.noreply.github.com>
2023-12-24 15:18:41 +0900

    役職表示部分を改善

--- src/app/pl/playing/state/RoleAssignment.tsx
+++ src/app/pl/playing/state/RoleAssignment.tsx
@@ -1,17 +1,39 @@
 import { useState } from 'react';
 import Button from '@/components/elements/button';
 import RoleName from '@/components/elements/roleName';
+import StateTitle from '@/components/elements/stateTitle';
 import DarkForestLayout from '@/components/layouts/darkForestLayout';
 import LogoHeader from '@/components/layouts/logoHeader';
 import PlayingFooter from '@/components/layouts/playingFooter';
 import { ButtonStyle } from '@/config/buttonStyle';
 import { FlexBaseLayoutStyle } from '@/config/flexBaseLayoutStyle';
+import { StateTitleStyle } from '@/config/stateTitleStyle';
 import styles from '@/styles/app/pl/playing/playing.module.scss';
 import { APIService } from '@/utils/apiService';
 import { DeviceIdService } from '@/utils/deviceIdService';
 
 export default function Home(): JSX.Element {
+  const [startCheck, setStartCheck] = useState(false);
   const [sendSuccess, setSendSuccess] = useState(false);
+
+  function buttonHandler() {
+    setStartCheck(true);
+  }
+
+  if (!startCheck) {
+    return (
+      <DarkForestLayout flexType={FlexBaseLayoutStyle.Top}>
+        <LogoHeader />
+        <StateTitle type={StateTitleStyle.Default} title={'役職確認'} />
+        <div className={styles.roleAssignmentStart}>
+          <div onClick={buttonHandler}>
+            <Button type={ButtonStyle.Blue}>確認する</Button>
+          </div>
+        </div>
+        <PlayingFooter />
+      </DarkForestLayout>
+    );
+  }
 
   async function sendRoleChecked() {
     const sendDeviceId = DeviceIdService.getToAPIData();
@@ -25,16 +47,32 @@ export default function Home(): JSX.Element {
     setSendSuccess(true);
   }
 
-  const OkButton = () => {
-    if (sendSuccess) {
-      return <>ほかのプレイヤーを待っています...</>;
-    }
+  function handleReCheckButton() {
+    setSendSuccess(false);
+  }
+
+  if (sendSuccess) {
     return (
-      <span onClick={sendRoleChecked}>
-        <Button type={ButtonStyle.Blue}>OK</Button>
-      </span>
+      <DarkForestLayout flexType={FlexBaseLayoutStyle.Top}>
+        <LogoHeader />
+        <div className={styles.flexInformation}>
+          <p>
+            他のプレイヤーを
+            <wbr />
+            待っています...
+          </p>
+        </div>
+        <ul>
+          <li>
+            <span onClick={handleReCheckButton}>
+              <Button type={ButtonStyle.Blue}>再確認</Button>
+            </span>
+          </li>
+        </ul>
+        <PlayingFooter />
+      </DarkForestLayout>
     );
-  };
+  }
 
   return (
     <DarkForestLayout flexType={FlexBaseLayoutStyle.Top}>
@@ -47,7 +85,9 @@ export default function Home(): JSX.Element {
       </div>
       <ul>
         <li>
-          <OkButton />
+          <span onClick={sendRoleChecked}>
+            <Button type={ButtonStyle.Blue}>OK</Button>
+          </span>
         </li>
       </ul>
       <PlayingFooter />
